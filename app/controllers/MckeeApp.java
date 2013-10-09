486245331ea9053ddbedf9771d7da364fa1e26f7
Joe Kent <thedeadlybutter@gmail.com>
2013-10-09 17:43:49 -0400

    Adding current Mckee mockup site progress

---
+++ app/controllers/MckeeApp.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Joe
+ * Date: 10/8/13
+ * Time: 6:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MckeeApp extends Controller {
+
+    public static Result cisco(){
+        return ok();
+    }
+
+}
