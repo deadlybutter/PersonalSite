611d43a3ccb03140c1b088d964817d11584d3bb2
Joe Kent <thedeadlybutter@gmail.com>
2013-11-29 23:26:57 -0500

    Redid the entire website, litterally

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,19 +8,31 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
 
-    public static Result mckeeSite(){
-        return ok(mckee.render());
+    public static Result computers(){
+        return ok(computer.render());
+    }
+
+    public static Result gamescoding(){
+        return ok(games.render());
+    }
+
+    public static Result web(){
+        return ok(web.render());
     }
 
     public static Result watlang(){
         return ok(watlang.render());
     }
 
-    public static Result cards(){
-        return ok(cards.render());
+    public static Result robot(){
+        return ok(robotics.render());
     }
-  
+
+    public static Result electronics(){
+        return ok(electronics.render());
+    }
+
 }
